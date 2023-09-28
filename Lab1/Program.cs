77fdcca4357eddbbdb89f02247310d40ad6d8b71
Galina Ignatova <igneous.galaxy003@gmail.com>
2023-09-28 11:12:02 +0300

    Unit Tests!

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,82 +1,15 @@
-﻿// Вариант 11
-// В одномерном массиве, состоящем из N вещественных элементов, вычислить:
-// номер максимального по модулю элемента массива
-// сумму элементов массива, расположенных после первого положительного элемента.
-// Преобразовать массив таким образом, чтобы сначала располагались все элементы, целая часть которых лежит в интервале [а, b], а потом — все остальные.
+﻿using CSharpLabs;
 
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-        Console.Write(array[i] + " ");
-    Console.WriteLine();
-}
+var l1 = new Lab1();
 
-int[] InitArray(int N)
-{
-    Random rnd = new Random();
-    int[] array = new int[N];
-    for (int i = 0; i < N; i++)
-        array[i] = rnd.Next(-10, 10);
-    
-    return array;
-}
-
-int FindAbsMaxIndex(int[] array)
-{
-    int absMax = 0;
-    int absMaxIndex = 0;
-    for (int i = 0; i < array.Length; i++)
-        if (Math.Abs(array[i]) > absMax)
-        {
-            absMax = Math.Abs(array[i]);
-            absMaxIndex = i;
-        }
-    
-    return absMaxIndex;
-}
-
-int FindSumAfterFirstPositive(int[] array)
-{
-    int sum = 0;
-    bool isItPositive = false;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (isItPositive)
-            sum += array[i];
-        if (array[i] > 0)
-            isItPositive = true;
-    }
-
-    return sum;
-}
-
-int[] RearrangeArray(int a, int b, int[] array)
-{
-    int[] AB = new int[array.Length];
-    int[] notAB = new int[array.Length];
-    int firstIndex = 0;
-    int secondIndex = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] >= a && array[i] <= b)
-            AB[firstIndex++] = array[i];
-        else
-            notAB[secondIndex++] = array[i];
-    }
-
-    AB = AB.Take(firstIndex).ToArray();
-    notAB = notAB.Take(secondIndex).ToArray();
-    array = AB.Concat(notAB).ToArray();
-    return array;
-}
-
-var tryParseN = int.TryParse(Console.ReadLine(), out var N);
+var tryParseN = uint.TryParse(Console.ReadLine(), out var N);
 while (!tryParseN)
-    tryParseN = int.TryParse(Console.ReadLine(), out N);
-int[] array = InitArray(N);
-PrintArray(array);
-Console.WriteLine(FindAbsMaxIndex(array));
-Console.WriteLine(FindSumAfterFirstPositive(array));
+    tryParseN = uint.TryParse(Console.ReadLine(), out N);
+
+int[] array = l1.InitArray((int) N);
+l1.PrintArray(array);
+Console.WriteLine(l1.FindAbsMaxIndex(array));
+Console.WriteLine(l1.FindSumAfterFirstPositive(array));
 
 var tryParseA = int.TryParse(Console.ReadLine(), out var a);
 while (!tryParseA)
@@ -84,5 +17,5 @@ while (!tryParseA)
 var tryParseB = int.TryParse(Console.ReadLine(), out var b);
 while (!tryParseB)
     tryParseB = int.TryParse(Console.ReadLine(), out b);
-array = RearrangeArray(a, b, array);
-PrintArray(array);
+array = l1.RearrangeArray(a, b, array);
+l1.PrintArray(array);
