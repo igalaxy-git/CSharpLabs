c68a3b11bad2a8fddbe2c1f04e2cadb1a3aebcd8
Galina Ignatova <igneous.galaxy003@gmail.com>
2023-09-28 09:42:08 +0300

    Add TryParse

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -5,13 +5,14 @@
 // Преобразовать массив таким образом, чтобы сначала располагались все элементы, целая часть которых лежит в интервале [а, b], а потом — все остальные.
 
 Random rnd = new Random();
-int N = Convert.ToInt32(Console.ReadLine());
+var tryParseN = int.TryParse(Console.ReadLine(), out var N);
+while (!tryParseN)
+    tryParseN = int.TryParse(Console.ReadLine(), out N);
 int[] array = new int[N];
 int absMax = 0;
 int absMaxIndex = 0;
 int sum = 0;
 bool isItPositive = false;
-
 for (int i = 0; i < N; i++)
 {
     array[i] = rnd.Next(-10, 10);
@@ -32,8 +33,14 @@ Console.WriteLine();
 Console.WriteLine(absMaxIndex);
 Console.WriteLine(sum);
 
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
+
+var tryParseA = int.TryParse(Console.ReadLine(), out var a);
+while (!tryParseA)
+    tryParseA = int.TryParse(Console.ReadLine(), out a);
+
+var tryParseB = int.TryParse(Console.ReadLine(), out var b);
+while (!tryParseB)
+    tryParseB = int.TryParse(Console.ReadLine(), out b);
 int[] AB = new int[N];
 int[] notAB = new int[N];
 int firstIndex = 0;
@@ -45,7 +52,6 @@ for (int i = 0; i < N; i++)
     else
         notAB[secondIndex++] = array[i];
 }
-
 AB = AB.Take(firstIndex).ToArray();
 notAB = notAB.Take(secondIndex).ToArray();
 array = AB.Concat(notAB).ToArray();
